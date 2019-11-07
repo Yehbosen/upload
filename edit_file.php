56166e7fb7779fdba05a8b1d3c6f0b9da0e2f6cd
Yehbosen <sulimase28@gmail.com>
2019-11-07 14:25:44 +0800

    更新檔案

---
+++ edit_file.php
@@ -0,0 +1,58 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=aaa";
+$pdo=new PDO($dsn,"root","1740");
+if(!empty($_FILES) && $_FILES['file']['error']==0){
+    $type=$_FILES['file']['type'];
+    $filename=$_FILES['file']['name'];
+    $path="./upload/";
+    $updateTime=date("Y-m-d H:i:s");
+    $id=$_POST['id'];
+    move_uploaded_file($_FILES['file']['tmp_name'] , $path . $filename);
+    //刪除原本的檔案
+    $sql="select * from files where id='$id'";
+    $origin=$pdo->query($sql)->fetch();
+    $origin_file=$origin['path'];
+    unlink($origin_file);
+    //更新資料
+    $sql="update files set name='$filename',type='$type',update_time='$updateTime',path='" . $path . $filename . "' where id='$id'";
+    $result=$pdo->exec($sql);
+    if($result==1){
+        echo "更新成功";
+        header("location:manage.php");
+    }else{
+        echo "DB有誤";
+    }
+}
+$id=$_GET['id'];
+$sql="select * from files where id='$id'";
+$data=$pdo->query($sql)->fetch();
+?>
+<form action="edit_file.php" method="post" enctype="multipart/form-data">
+<table>
+    <tr>
+        <td colspan="2">
+            <img src="<?=$data['path'];?>" style="width:200px;height:200px">
+        </td>
+
+    </tr>
+    <tr>
+        <td>name</td>
+        <td><?=$data['name'];?></td>
+    </tr>
+    <tr>
+        <td>path</td>
+        <td><?=$data['path'];?></td>
+    </tr>
+    <tr>
+        <td>type</td>
+        <td><?=$data['type'];?></td>
+    </tr>
+    <tr>
+        <td>create_time</td>
+        <td><?=$data['create_time'];?></td>
+    </tr>
+</table>
+更新檔案:<input type="file" name="file"><br>
+<input type="hidden" name="id" value="<?=$data['id'];?>">
+<input type="submit" value="更新">
+</form>
--- manage.php
+++ manage.php
@@ -5,8 +5,23 @@
  * 3.取得檔案資訊並寫入資料表
  * 4.製作檔案管理功能頁面
  */
-
-
+$dsn="mysql:host=localhost;charset=utf8;dbname=aaa";
+$pdo=new PDO($dsn,"root","1740");
+if(!empty($_FILES) && $_FILES['file']['error']==0){
+   
+    $type=$_FILES['file']['type'];
+    $filename=$_FILES['file']['name'];
+    $path="./upload/";
+    
+    move_uploaded_file($_FILES['file']['tmp_name'] , $path . $filename);
+    $sql="insert into files (`name`,`type`,`path`) values('$filename','$type','" . $path . $filename . "')";
+    $result=$pdo->exec($sql);
+    if($result==1){
+        echo "上傳成功";
+    }else{
+        echo "DB有誤";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,18 +31,60 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案管理功能</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+    a{
+        display: inline-block;
+        border: 1px solid #ccc;
+        padding: 5px 15px;
+        border-radius: 20px;
+        box-shadow: 1px 1px 3px #ccc;
+    }
+    
+    </style>
 </head>
 <body>
 <h1 class="header">檔案管理練習</h1>
 <!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
-
-
-
-
+<form action="manage.php" method="post" enctype="multipart/form-data">
+  檔案：<input type="file" name="file" ><br>
+  <input type="submit" value="上傳">
+</form>
 
 <!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
 
+<table>
+    <tr>
+        <td>id</td>
+        <td>name</td>
+        <td>type</td>
+        <td>path</td>
+        <td>create time</td>
+        <td>操作</td>
+    </tr>
 
+    <?php
+        $sql="select * from files";
+        $rows=$pdo->query($sql)->fetchAll();
+        foreach ($rows as $key => $file) {  
+    ?>
+    <tr>
+        <td><?=$file['id'];?></td>
+        <td><?=$file['name'];?></td>
+        <td><?=$file['type'];?></td>
+        <td><?=$file['path'];?></td>
+        <td><?=$file['create_time'];?></td>
+        <td>
+            <a href="edit_file.php?id=<?=$file['id'];?>">更新檔案</a>
+            <a href="del_file.php?id=<?=$file['id'];?>">刪除檔案</a>
+        
+        </td>
+    </tr>
+   
+    <?php
+     }
+    ?>
+
+</table>
 
 
 </body>
